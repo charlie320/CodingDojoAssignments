3b73e1be472b588ac971a507472f753ff50c94fb
Your Name <Charlie@Charlies-MacBook-Pro.local>
2017-07-21 10:56:15 -0500

    courses update

--- test_app/apps/test_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^blogs$', views.blogs),
-    url(r'^comments/(?P<id>\d+)$', views.comments)
-]
