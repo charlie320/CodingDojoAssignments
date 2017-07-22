e53020ea515f373917bcf542465e0b7d28e74b35
Your Name <Charlie@Charlies-MBP.home>
2017-07-22 16:27:20 -0500

    updated project

--- username_validation/apps/username_validation_app/models.py
+++ username_validation/apps/username_validation_app/models.py
@@ -14,26 +14,7 @@ class UserManager(models.Manager):
                 if is_valid:
                     return True
 
-        # if len(email) > 8:
-        #     return ("This is a valid email according to min length(8)")
-        # if len(email) < 26:
-        #     return ("This is a valid email according to max length(26)")
-        # if is_valid:
-        #     return ("This is a valid email")
         return False
-        # if request.method == "POST" then continue
-        # if len(email) > 8 and < 26 then continue, else invalid
-        # if email matches some letters + @ + some letters + . + 3 letters then valid
-
-
-        # print "Running a login function!"
-        # print "If successful login occurs, maybe return {'theuser':user} where user is a user object?"
-        # print "If unsuccessful, maybe return { 'errors':['Login unsuccessful'] }"
-        # print email
-        # myString = email + " and more words"
-
-            #return ("This is an invalid email")
-
 
     def register(self, postData):
         print ("Register a user here")
@@ -49,6 +30,3 @@ class User(models.Model):
     # *************************
     # Connect an instance of UserManager to our User model overwriting
     # the old hidden objects key with a new one with extra properties!!!
-    #objects = UserManager()
-
-# Create your models here.
--- username_validation/apps/username_validation_app/views.py
+++ username_validation/apps/username_validation_app/views.py
@@ -3,15 +3,22 @@ from .models import User
 # Create your views here.
 
 def index(request):
-
-    return render(request, 'username_validation_app/index.html')
+    context = {
+        "messages" : ""
+    }
+    return render(request, 'username_validation_app/index.html', context)
 
 def validate(request):
     request.session['name'] = request.POST['username']
     myResponse = User.userManager.login(request.POST['username'])
     print myResponse
-
-    return render(request, 'username_validation_app/success.html')
+    if myResponse:
+        return render(request, 'username_validation_app/success.html')
+    else:
+        context = {
+            "messages" : "Username is not valid!"
+        }
+        return render(request, 'username_validation_app/index.html', context)
 
 def return_home(request):
     # print "You made it to return home"
