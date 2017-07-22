6edb4234f749c85c119c38b16b1ce86a597341c3
Your Name <Charlie@Charlies-MBP.home>
2017-07-22 15:10:13 -0500

    updated username_validation

--- username_validation/apps/username_validation_app/models.py
+++ username_validation/apps/username_validation_app/models.py
@@ -1,5 +1,54 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from validate_email import validate_email
+#Our new manager!
+#No methods in our new manager should ever catch the whole request object with a
+#parameter!!! (just parts, like request.POST)
+
+class UserManager(models.Manager):
+    def login(self, email):
+        is_valid = validate_email(email)
+
+        if len(email) > 8:
+            if len(email) < 26:
+                if is_valid:
+                    return True
+
+        # if len(email) > 8:
+        #     return ("This is a valid email according to min length(8)")
+        # if len(email) < 26:
+        #     return ("This is a valid email according to max length(26)")
+        # if is_valid:
+        #     return ("This is a valid email")
+        return False
+        # if request.method == "POST" then continue
+        # if len(email) > 8 and < 26 then continue, else invalid
+        # if email matches some letters + @ + some letters + . + 3 letters then valid
+
+
+        # print "Running a login function!"
+        # print "If successful login occurs, maybe return {'theuser':user} where user is a user object?"
+        # print "If unsuccessful, maybe return { 'errors':['Login unsuccessful'] }"
+        # print email
+        # myString = email + " and more words"
+
+            #return ("This is an invalid email")
+
+
+    def register(self, postData):
+        print ("Register a user here")
+        print ("If successful, maybe return {'theuser':user} where user is a user object?")
+        print ("If unsuccessful do something like this? return {'errors':['User first name to short', 'Last name too short'] ")
+
+class User(models.Model):
+    email = models.CharField(max_length = 45)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    userManager = UserManager()
+    # *************************
+    # Connect an instance of UserManager to our User model overwriting
+    # the old hidden objects key with a new one with extra properties!!!
+    #objects = UserManager()
 
 # Create your models here.
--- username_validation/apps/username_validation_app/views.py
+++ username_validation/apps/username_validation_app/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, HttpResponse
+from .models import User
 # Create your views here.
 
 def index(request):
+
     return render(request, 'username_validation_app/index.html')
 
 def validate(request):
-    print "You made it to the validate function"
+    request.session['name'] = request.POST['username']
+    myResponse = User.userManager.login(request.POST['username'])
+    print myResponse
+
     return render(request, 'username_validation_app/success.html')
 
 def return_home(request):
-    print "You made it to return home"
+    # print "You made it to return home"
     return redirect('/')
