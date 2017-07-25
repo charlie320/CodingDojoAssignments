cfcffd49473a136c69669b3f465aa3f3edda692e
Your Name <Charlie@Charlies-MBP.home>
2017-07-25 11:17:46 -0500

    updated ds3

--- dojo_secrets_2/apps/login_app/models.py
+++ dojo_secrets_2/apps/login_app/models.py
@@ -1,5 +1,18 @@
 from __future__ import unicode_literals
-
 from django.db import models
+import bcrypt
 
 # Create your models here.
+class UserManager(models.Manager):
+    pass
+
+class User(models.Model):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    email = models.EmailField()
+    password = models.CharField(max_length = 255)
+    friends = models.ManyToManyField('self', related_name = 'friend_by')
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    objects = UserManager()
--- dojo_secrets_2/apps/login_app/views.py
+++ dojo_secrets_2/apps/login_app/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+import bcrypt
+#from .models import User
 
 # Create your views here.
+def flashErrors(request, errors):
+    for error in errors:
+        messages.error(request. error)
+
 def index(request):
     return render(request, 'login_app/index.html')
