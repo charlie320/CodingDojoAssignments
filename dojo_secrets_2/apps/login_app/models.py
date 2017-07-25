e8c71c2b1a0d48185dd7ff549ef47092e1448663
Your Name <Charlie@Charlies-MBP.home>
2017-07-25 11:42:13 -0500

    updated ds2

--- dojo_secrets_2/apps/login_app/models.py
+++ dojo_secrets_2/apps/login_app/models.py
@@ -8,6 +8,45 @@ class UserManager(models.Manager):
         id = request.session['user_id']
         return User.objects.get(id=id)
 
+    def validateRegistration(self, form_date):
+        errors = []
+
+        if len(form_data)['first_name'] == 0:
+            errors.append("First Name is required.")
+        if len(form_data)['last_name'] == 0:
+            errors.append("Last Name is required.")
+        if len(form_data)['email'] == 0:
+            errors.append("Email is required.")
+        if len(form_data)['password'] == 0:
+            errors.append("Password is required.")
+        if form_data['password'] != form_data['password_confirmation']:
+            errors.append("Passwords do not match.")
+
+        return errors
+
+    def validateLogin(self, form_data):
+        errors = []
+
+        if len(form_data)['email'] == 0:
+            errors.append("Email is required.")
+        if len(form_data)['password'] == 0:
+            errors.append("Password is required.")
+
+        return errors
+
+    def createUser(self, form_data):
+        password = str(form_data['password'])
+        hashed_pw = bcrypt.hashpw(password, bcrypt.gensalt())
+
+        user = User.objects.create(
+            first_name = form_data['first_name'],
+            last_name = form_data['last_name'],
+            email = form_data['email'],
+            password = hashed_pw,
+        )
+        return user
+
+
 class User(models.Model):
     first_name = models.CharField(max_length = 255)
     last_name = models.CharField(max_length = 255)
