64625393a6cf5c2ebb14ed28a339fd251d331299
Your Name <Charlie@Charlies-MacBook-Pro.local>
2017-07-26 09:51:14 -0500

    completed ds2

--- dojo_secrets_2/apps/login_app/views.py
+++ dojo_secrets_2/apps/login_app/views.py
@@ -6,7 +6,7 @@ from .models import User
 # Create your views here.
 def flashErrors(request, errors):
     for error in errors:
-        messages.error(request. error)
+        messages.error(request, error)
 
 def index(request):
     return render(request, 'login_app/index.html')
