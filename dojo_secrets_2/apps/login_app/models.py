949f0e7888e13e90d13baf86e1b12670cd174b5e
Your Name <Charlie@Charlies-MBP.home>
2017-07-26 00:19:46 -0500

    completed ds2

--- dojo_secrets_2/apps/login_app/models.py
+++ dojo_secrets_2/apps/login_app/models.py
@@ -8,16 +8,16 @@ class UserManager(models.Manager):
         id = request.session['user_id']
         return User.objects.get(id=id)
 
-    def validateRegistration(self, form_date):
+    def validateRegistration(self, form_data):
         errors = []
 
-        if len(form_data)['first_name'] == 0:
+        if len(form_data['first_name']) == 0:
             errors.append("First Name is required.")
-        if len(form_data)['last_name'] == 0:
+        if len(form_data['last_name']) == 0:
             errors.append("Last Name is required.")
-        if len(form_data)['email'] == 0:
+        if len(form_data['email']) == 0:
             errors.append("Email is required.")
-        if len(form_data)['password'] == 0:
+        if len(form_data['password']) == 0:
             errors.append("Password is required.")
         if form_data['password'] != form_data['password_confirmation']:
             errors.append("Passwords do not match.")
@@ -27,9 +27,9 @@ class UserManager(models.Manager):
     def validateLogin(self, form_data):
         errors = []
 
-        if len(form_data)['email'] == 0:
+        if len(form_data['email']) == 0:
             errors.append("Email is required.")
-        if len(form_data)['password'] == 0:
+        if len(form_data['password']) == 0:
             errors.append("Password is required.")
 
         return errors
@@ -50,7 +50,7 @@ class UserManager(models.Manager):
 class User(models.Model):
     first_name = models.CharField(max_length = 255)
     last_name = models.CharField(max_length = 255)
-    email = models.EmailField()
+    email = models.EmailField(max_length=255)
     password = models.CharField(max_length = 255)
     friends = models.ManyToManyField('self', related_name = 'friend_by')
     created_at = models.DateTimeField(auto_now_add = True)
--- dojo_secrets_2/apps/login_app/views.py
+++ dojo_secrets_2/apps/login_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 import bcrypt
-#from .models import User
+from .models import User
 
 # Create your views here.
 def flashErrors(request, errors):
@@ -41,16 +41,51 @@ def register(request):
 
 def login(request):
     print "Inside the login function"
-    return redirect('/')
+    if request.method == "POST":
+        errors = User.objects.validateLogin(request.POST)
+
+        if not errors:
+            user = User.objects.filter(email = request.POST['email']).first()
+
+            if user:
+                password = str(request.POST['password'])
+                user_password = str(user.password)
+                hashed_pw = bcrypt.hashpw(password, user_password)
+
+                if hashed_pw == user.password:
+                    request.session['user_id'] = user.id
+                    return redirect(reverse('success'))
+
+            errors.append("Invalid account information.")
+
+        flashErrors(request, errors)
+
+    return redirect(reverse('landing'))
+
 
 def logout(request):
     print "Inside the logout function"
-    pass
+    if 'user_id' in request.session:
+        request.session.pop('user_id')
+    return redirect(reverse('landing'))
 
 def addFriend(request, id):
     print "Inside the addFriend function"
-    pass
+    if request.method == "POST":
+        if 'user_id' in request.session:
+            current_user = User.objects.currentUser(request)
+            friend = User.objects.get(id=id)
+            current_user.friends.add(friend)
+            return redirect(reverse('success'))
+    return redirect(reverse('landing'))
 
 def removeFriend(request, id):
     print "Inside the removeFriend function"
-    pass
+    if request.method == "POST":
+        if 'user_id' in request.session:
+            current_user = User.objects.currentUser(request)
+            friend = User.objects.get(id=id)
+            current_user.friends.remove(friend)
+            return redirect(reverse('success'))
+
+    return redirect(reverse('landing'))
